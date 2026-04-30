087a22a69a0fc29951e5d202d9142742fe5aa60c
bintang2629 <bimbimpratama23@gmail.com>
2026-04-30 20:14:49 +0700

    nodebaru

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -47,5 +47,26 @@ public:
             START = nodeBaru;
             return;
         }
+    Node *current = START;
+
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "Duplicate roll numbers not allowed" << endl;
+            return;
+        }
+
+        nodeBaru->next = current->next;
+        nodeBaru->prev = current;
+
+        if (current->next != NULL)
+            current->next->prev = nodeBaru;
+
+        current->next = nodeBaru;
     }
-};       
+
+}; 
